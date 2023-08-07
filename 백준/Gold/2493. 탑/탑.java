2622bb5ba1979778aff666f8c46746d30962738d
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2023-08-07 21:20:15 +0900

    [Gold V] Title: 탑, Time: 780 ms, Memory: 87180 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -6,26 +6,25 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
 		StringTokenizer st = null;
+		
+		int n = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		Stack<int[]> stack = new Stack<>();
 		stack.push(new int[] {100000001, 0});
 		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= n; i++) {
+		for (int i = 0; i < n; i++) {
 			int h = Integer.parseInt(st.nextToken());
-			while (stack.peek()[0] < h) {
+			while (!stack.isEmpty() && stack.peek()[0] <= h) {
 				stack.pop();
 			}
 			sb.append(stack.peek()[1]).append(" ");
-			stack.push(new int[] {h, i});
-		}
+			stack.push(new int[] {h, i + 1});
+		}	
 		sb.append("\n");
 		System.out.println(sb.toString());
 	}
-
 }
