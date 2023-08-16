e436384bfed37f4b65ade5ef2757fd6f3795fb1c
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2023-08-16 20:07:14 +0900

    [Silver IV] Title: 좋은 단어, Time: 324 ms, Memory: 23052 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -4,24 +4,29 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            String[] word = br.readLine().split("");
-            Stack<String> stack = new Stack<>();
-            for (String w : word) {
-                if (!stack.isEmpty() && w.equals(stack.peek())) {
-                    stack.pop();
-                } else {
-                    stack.push(w);
-                }
-            }
-            if (stack.isEmpty()) {
-                answer += 1;
-            }
-        }
-        System.out.println(answer);
-    }
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int result = 0;
+		for (int t = 0; t < N; t++) {
+			char[] word = br.readLine().toCharArray();
+			Stack<Character> stack = new Stack<>();
+
+			for (char w : word) {
+				if (!stack.isEmpty() && w == stack.peek()) {
+					stack.pop();
+				} else {
+					stack.push(w);
+				}
+			}
+			
+			if (stack.isEmpty()) {
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
 }
