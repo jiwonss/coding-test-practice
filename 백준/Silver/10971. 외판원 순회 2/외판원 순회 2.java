a8a967f1ec2df66a7442aa73dfec64d576c95a9f
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2023-08-30 17:46:25 +0900

    [Silver II] Title: 외판원 순회 2, Time: 136 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -11,12 +11,13 @@ public class Main {
 	static int[] visited;
 	static int[][] W;
 
-	private static void dfs(int k, int cost, Deque<Integer> dq) {
-		if (result <= cost) return;
+	private static void dfs(int k, int cost, int last) {
+		if (result <= cost) return; // 가지치기
 		
-		int last = dq.peekLast();
+		// 제일 최근에 방문한 도시
 		if (k == N - 1) {
-			int start = dq.peekFirst();
+			int start = 0; // 처음 방문한 도시
+			// 제일 마지막에 방문한 도시에서 처음 방문한 도시의 비용이 0이 아닌 경우, result 갱신
 			if (W[last][start] != 0) {
 				result = Math.min(result, cost + W[last][start]);
 			}
@@ -26,10 +27,8 @@ public class Main {
 		for (int i = 1; i < N; i++) {
 			if (visited[i] == 1 || W[last][i] == 0) continue;
 			visited[i] = 1;
-			dq.offer(i);
-			dfs(k + 1, cost + W[last][i], dq);
+			dfs(k + 1, cost + W[last][i], i);
 			visited[i] = 0;
-			dq.pollLast();
 		}
 	}
 	
@@ -39,6 +38,7 @@ public class Main {
 		
 		N = Integer.parseInt(br.readLine());
 		
+		// 도시 i에서 도시 j로 가기 위한 비용 저장
 		W = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -54,7 +54,7 @@ public class Main {
 		visited[0] = 1;
 		dq.offer(0);
 		
-		dfs(0, 0, dq);
+		dfs(0, 0, 0);
 		System.out.println(result);
 	}
 
