d86ca0f44bbdeee237cb5e83cc31f22a337f13d4
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2023-10-04 15:51:39 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,517 ms, Memory: 97,060 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.py
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.py
@@ -5,17 +5,17 @@ for t in range(1, T + 1):
     N = int(input())
     M = int(input())
 
-    graph1 = [[] for _ in range(N + 1)]
-    graph2 = [[] for _ in range(N + 1)]
+    graph1, graph2 = [[] for _ in range(N + 1)], [[] for _ in range(N + 1)]
     for _ in range(M):
         a, b = map(int, input().split())
-        graph1[a].append(b)
-        graph2[b].append(a)
+        graph1[a].append(b) # a -> b
+        graph2[b].append(a) # b -> a
 
     result = 0
+    q1, q2 = deque(), deque()
     for i in range(1, N + 1):
-        q1, q2 = deque(), deque()
         visited1, visited2 = [0] * (N + 1), [0] * (N + 1)
+        # i번 학생보다 키가 큰 학생 찾기
         q1.append(i)
         while q1:
             x = q1.popleft()
@@ -23,7 +23,7 @@ for t in range(1, T + 1):
                 if not visited1[v]:
                     visited1[v] = 1
                     q1.append(v)
-
+        # i번 학생보다 키가 작은 학생 찾기
         q2.append(i)
         while q2:
             x = q2.popleft()
@@ -31,7 +31,8 @@ for t in range(1, T + 1):
                 if not visited2[v]:
                     visited2[v] = 1
                     q2.append(v)
-        
+        # i번 학생보다 키가 큰 학생 수 + i번 학생보다 키가 작은 학생 수
+        # 더한 값이 N - 1일 경우 자신의 키가 몇 번째인지 정확히 할 수 있다.
         if sum(visited1) + sum(visited2) == N - 1:
             result += 1
     print(f'#{t} {result}')
