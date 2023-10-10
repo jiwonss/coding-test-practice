6fd15ddf75abe5fa832a6003131a2202cac8826f
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2023-10-10 12:00:57 +0900

    [Gold V] Title: AC, Time: 912 ms, Memory: 106088 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -2,64 +2,78 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static StringBuilder sb = new StringBuilder();
+	static int n, reverse;
+	static boolean error;
+	static String[] p;
+	static Deque<String> dq;
+	static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String[] p = br.readLine().split("");
-            int n = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine(), "[],");
-            ArrayDeque<Integer> dq = new ArrayDeque<>();
-            for (int i = 0; i < n; i++) {
-                dq.add(Integer.parseInt(st.nextToken()));
-            }
-            AC(p, dq);
-        }
-        System.out.println(sb);
-    }
-    
-    public static void AC(String[] p, ArrayDeque<Integer> dq) {
-        int reverse = -1;
-        for (int i = 0; i < p.length; i++) {
-            if (p[i].equals("R")) {
-                reverse *= (-1);
-            } else {
-                if (dq.isEmpty()) {
-                    sb.append("error\n");
-                    return;
-                }
-                if (reverse == 1) {
-                    dq.pollLast();
-                } else {
-                    dq.pollFirst();
-                }
-            }
-        }
-        print(reverse, dq);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
 
-    public static void print(int reverse, ArrayDeque<Integer> dq) {
-        sb.append("[");
-        if (dq.size() > 0) {
-            if (reverse == 1) {
-                sb.append(dq.pollLast());
-                while (!dq.isEmpty()) {
-                    sb.append(",").append(dq.pollLast());
-                }
-            } else {
-                sb.append(dq.pollFirst());
-                while (!dq.isEmpty()) {
-                    sb.append(",").append(dq.pollFirst());
-                }
-            }
-        }
-        sb.append("]").append("\n");
-    }
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			p = br.readLine().split("");
+			n = Integer.parseInt(br.readLine());
+
+			st = new StringTokenizer(br.readLine(), "[],");
+			dq = new ArrayDeque<>();
+			for (int i = 0; i < n; i++) {
+				dq.offer(st.nextToken());
+			}
+
+			error = false;
+			ac();
+			print();
+		}
+		System.out.println(sb);
+	}
+
+	private static void ac() {
+		reverse = 1;
+		for (int i = 0; i < p.length; i++) {
+			if (p[i].equals("R")) {
+				reverse *= -1;
+			} else {
+				if (dq.isEmpty()) {
+					error = true;
+					return;
+				}
+				if (reverse == 1) {
+					dq.pollFirst();
+				} else {
+					dq.pollLast();
+				}
+			}
+		}
+	}
+	
+	private static void print() {
+		if (error) {
+			sb.append("error").append("\n");
+		} else {
+			sb.append("[");
+			if (dq.size() != 0) {
+				if (reverse == 1) {
+					sb.append(dq.pollFirst());
+					while (!dq.isEmpty()) {
+						sb.append(",").append(dq.pollFirst());
+					}
+				} else {
+					sb.append(dq.pollLast());
+					while (!dq.isEmpty()) {
+						sb.append(",").append(dq.pollLast());
+					}
+				}				
+			}
+			sb.append("]").append("\n");
+		}
+	}
+
 }
