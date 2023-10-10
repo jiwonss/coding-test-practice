f56fb880f63876cebef97e0dd53f918cac7f5d3f
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2023-10-10 12:14:38 +0900

    [Gold V] Title: 토마토, Time: 600 ms, Memory: 102800 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -6,68 +6,62 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int n, m;
+
+	static int N, M, result;
 	static int[][] box;
-	static Queue<Point> q = new ArrayDeque<>();
-	static int[] dx = {1, 0, -1, 0}, dy = {0, 1, 0, -1};
-	
-	static class Point {
-		private int x;
-		private int y;
-		
-		public Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-	
-	private static void bfs() {
-		while (!q.isEmpty()) {
-			Point p = q.poll();
-			for (int d = 0; d < 4; d++) {
-				int nx = p.x + dx[d];
-				int ny = p.y + dy[d];
-				if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-				if (box[nx][ny] != 0) continue;
-				box[nx][ny] = box[p.x][p.y] + 1;
-				q.offer(new Point(nx, ny));
+	static Queue<int[]> q;
+	static int[] dx = { 1, 0, -1, 0 }, dy = { 0, 1, 0, -1 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+
+		q = new ArrayDeque<>();
+		box = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				box[i][j] = Integer.parseInt(st.nextToken());
+				if (box[i][j] == 1) {
+					q.offer(new int[] { i, j });
+				}
 			}
 		}
+
+		bfs();
+
+		result = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (box[i][j] == 0) {
+					System.out.println(-1);
+					return;
+				}
+				if (result < box[i][j]) result = box[i][j];
+			}
+		}
+		System.out.println(result - 1);
 	}
-	
-	public static void main(String[] args) throws IOException {
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 StringTokenizer st = null;
-		 String[] temp = br.readLine().split(" ");
-		 m = Integer.parseInt(temp[0]);
-		 n = Integer.parseInt(temp[1]);
-		 box = new int[n][m];
-		 for (int i = 0; i < n; i++) {
-			 st = new StringTokenizer(br.readLine());
-			 for (int j = 0; j < m; j++) {
-				 box[i][j] = Integer.parseInt(st.nextToken());
-				 if (box[i][j] == 1) {
-					 q.offer(new Point(i, j));
-				 }
-			 }
-		 }
-		 
-		 bfs();
-		 
-		 int result = 0;
-		 for (int i = 0; i < n; i++) {
-			 for (int j = 0; j < m; j++) {
-				 if (box[i][j] == 0) {
-					 System.out.println(-1);
-					 return;
-				 }
-				 if (result < box[i][j]) {
-					 result = box[i][j];
-				 }
-			 }
-		 }
-		 System.out.println(result - 1);
+
+	private static void bfs() {
+		while (!q.isEmpty()) {
+			int[] pos = q.poll();
+			int x = pos[0], y = pos[1];
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+					continue;
+				if (box[nx][ny] != 0)
+					continue;
+				box[nx][ny] = box[x][y] + 1;
+				q.offer(new int[] { nx, ny });
+			}
+		}
 	}
 
 }
