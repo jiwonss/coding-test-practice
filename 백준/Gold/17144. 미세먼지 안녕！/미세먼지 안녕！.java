5e7cbc1ecb6f7b589b4a6ab900809aa4602556bb
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-02-03 20:35:17 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 384 ms, Memory: 109112 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,140 +1,80 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int R, C, T, result;
+	
+	/**
+	 * x, y : 좌표
+	 * v : 좌표에 존재하는 미세먼지 양
+	 */
+	static class Point {
+		int x; int y; int v;
+		Point(int x, int y, int v) {
+			this.x = x;
+			this.y = y;
+			this.v = v;
+		}
+	}
+	
+	static BufferedReader br;
+	static StringTokenizer st;
+	
+	static int R, C, T, up, down, result;
 	static int[][] map;
-	static boolean[][] visited;
-	static Queue<int[]> q;
-	static List<int[]> list;
-	static List<Integer> airCleaner;
+	static List<Point> findDustList, spreadInfo;
 	static int[] dx = {1, 0, -1, 0}, dy = {0, 1, 0, -1};
-	static int[] aux = {-1, 0, 1, 0}, auy = {0, 1, 0, -1};
-	static int[] adx = {1, 0, -1, 0}, ady = {0, 1, 0, -1};
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		
+		init();
+		monitoring();
+		monitoringResult();
+		System.out.println(result);
+	}
+	
+	public static void init() throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+
 		st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		T = Integer.parseInt(st.nextToken());
 		
 		map = new int[R][C];
-		airCleaner = new ArrayList<>();
 		for (int i = 0; i < R; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < C; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == -1) {
-					airCleaner.add(i);
-				}
+			}
+		}
+		
+		br.close();
+	}
+	
+	// 미세먼지 실시간 모니터링
+	public static void monitoring() {
+		// 공기청정기 위치 찾기
+		up = 0; down = 0;
+		for (int i = 0; i < R; i++) {
+			if (map[i][0] == -1) {
+				up = i;
+				down = i + 1;
+				break;
 			}
 		}
 		
 		for (int i = 0; i < T; i++) {
-			getPM();
-			spread();
-			run();
-		}
-		
-		result = totalPM();
-		System.out.println(result);
-	}
-	
-	// 미세먼지 저장
-	private static void getPM() {
-		q = new ArrayDeque<>();
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				if (map[i][j] > 0) {
-					q.offer(new int[] {i, j});
-				}
-			}
+			spreadFineDust();
+			runAirCleaner();
 		}
 	}
 	
-	// 미세먼지 확산
-	private static void spread() {
-		list = new ArrayList<>();
-		visited = new boolean[R][C];
-		while (!q.isEmpty()) {
-			int[] pos = q.poll();
-			int x = pos[0], y = pos[1];
-			int cnt = 0;
-			for (int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
-				if (map[nx][ny] == -1) continue;
-				cnt++;
-				list.add(new int[] {nx, ny, map[x][y] / 5});
-			}
-			list.add(new int[] {x, y, -(map[x][y] / 5) * cnt});
-		}
-		
-		for (int i = 0; i < list.size(); i++) {
-			int[] temp = list.get(i);
-			int x = temp[0], y = temp[1], v = temp[2];
-			map[x][y] = map[x][y] + v;
-		}
-	}
-	
-	// 공기청정기 작동
-	private static void run() {
-		int x, y, idx, d;
-		// 위쪽 공기청정기 바람(상, 우, 하, 좌)
-		idx = airCleaner.get(0);
-		x = idx; y = 0;
-		d = 0;
-		while (true) {
-			int nx = x + aux[d];
-			int ny = y + auy[d];
-			if (nx < 0 || nx > idx || ny < 0 || ny >= C) {
-				d = (d + 1) % 4;
-				continue;
-			}
-			if (map[x][y] != -1) {
-				map[x][y] = map[nx][ny];
-				map[nx][ny] = 0;
-			}
-			x = nx; y = ny;
-			if (x == idx && y == 1) break;
-		}
-		
-		// 아래쪽 공기청정기 바람(하, 우, 상, 좌)
-		idx = airCleaner.get(1);
-		x = idx; y = 0;
-		d = 0;
-		while (true) {
-			int nx = x + adx[d];
-			int ny = y + ady[d];
-			if (nx < idx || nx >= R || ny < 0 || ny >= C) {
-				d = (d + 1) % 4;
-				continue;
-			}
-			if (map[x][y] != -1) {
-				map[x][y] = map[nx][ny];
-				map[nx][ny] = 0;
-			}
-			x = nx; y = ny;
-			if (x == idx && y == 1) break;
-		}
-		
-	}
-	
-	// 남은 미세먼지 양 
-	private static int totalPM() {
-		int result = 0;
+	// 모니터링 결과
+	public static void monitoringResult() {
+		result = 0;
 		for (int i = 0; i < R; i++) {
 			for (int j = 0; j < C; j++) {
 				if (map[i][j] > 0) {
@@ -142,7 +82,97 @@ public class Main {
 				}
 			}
 		}
-		return result;
+	}
+	
+	
+	// 미세먼지 확산
+	public static void spreadFineDust() {
+		// 현재 존재하는 미세먼지 리스트에 저장
+		findDustList = new ArrayList<>();
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if (map[i][j] > 0) {
+					findDustList.add(new Point(i, j, map[i][j]));
+				}
+			}
+		}
+		
+		// 미세먼지 확산 정보 저장
+		for (Point findDust : findDustList) {
+			int cnt = 0;
+			int amount = (int) (findDust.v / 5);
+			for (int i = 0; i < 4; i++) {
+				int nx = findDust.x + dx[i];
+				int ny = findDust.y + dy[i];
+				if (outOfBounds(nx, ny) || map[nx][ny] == -1) continue;
+				cnt++;
+				map[nx][ny] += amount;
+			}
+			map[findDust.x][findDust.y] -= (amount * cnt);
+		}
+	}
+	
+	public static boolean outOfBounds(int x, int y) {
+		if (x < 0 || x >= R || y < 0 || y >= C) return true;
+		return false;
+	}
+	
+	
+	// 공기청정기 작동
+	public static void runAirCleaner() {
+		// 위쪽 공기청정기 작동
+		int d;
+		int upX = up, upY = 0;
+		for (int i = 0; i < 4; i++) {
+			if (i == 0) d = 2;
+			else if (i == 2) d = 0;
+			else d = i;
+			int temp = 0;
+			int nx = upX, ny = upY + 1;
+			while (true) {
+				nx = upX + dx[d];
+				ny = upY + dy[d];
+				if (outOfBounds(nx, ny) || nx > up) break;
+				if (nx == up && ny == 0) {
+					map[up][0] = -1;
+					break;
+					}
+				if (map[nx][ny] <= 0) {
+					temp = 0;
+					upX = nx; upY = ny;
+					continue;
+				}
+				temp = map[upX][upY];					
+				map[upX][upY] = map[nx][ny];
+				map[nx][ny] = 0;
+				upX = nx; upY = ny;
+			}
+		}
+		
+		// 아래쪽 공기청정기 작동
+		int downX = down, downY = 0;
+		for (int i = 0; i < 4; i++) {
+			int temp = 0;
+			int nx = downX, ny = downY;
+			while (true) {
+				nx = downX + dx[i];
+				ny = downY + dy[i];
+				if (outOfBounds(nx, ny) || nx < down) break;
+				if (nx == down && ny == 0) {
+					map[down][0] = -1;
+					break;
+				}
+				if (map[nx][ny] <= 0) {
+					temp = 0;
+					downX = nx; downY = ny;
+					continue;
+				}
+				temp = map[downX][downY];
+				map[downX][downY] = map[nx][ny];
+				map[nx][ny] = 0;
+				downX = nx; downY = ny;
+			}
+		}
 	}
 
 }
