16f3b1315d3f62af188488ccd152ae31e69dd1c2
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-02-05 23:10:41 +0900

    [Silver II] Title: 침투, Time: 840 ms, Memory: 93272 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.java
+++ 백준/Silver/13565. 침투/침투.java
@@ -14,7 +14,7 @@ public class Main {
 		}
 	}
 	
-	static BufferedReader br;
+	static BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
 	static StringTokenizer st;
 	
 	static int N, M;
@@ -25,13 +25,10 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		init();
-		bfs();
-		System.out.println(check());
+		System.out.println(solve());
 	}
 	
 	public static void init() throws IOException {
-		br = new BufferedReader(new java.io.InputStreamReader(System.in));
-		
 		st = new StringTokenizer(br.readLine());
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
@@ -43,45 +40,40 @@ public class Main {
 				map[i][j] = temp[j].charAt(0) - '0';
 			}
 		}
-		
-		br.close();
 	}
 	
-	public static void bfs() {
-		q = new ArrayDeque<>();
+	public static String solve() {
 		visited = new boolean[M][N];
 		for (int i = 0; i < N; i++) {
-			if (map[0][i] == 0) {
-				q.offer(new Point(0, i));
-				visited[0][i] = true;
-			}
+			if (map[0][i] == 1) continue;
+			if (bfs(0, i)) return "YES";
 		}
+		return "NO";
+	}
+	
+	public static boolean bfs(int x, int y) {
+		q = new ArrayDeque<>();
+		q.offer(new Point(x, y));
+		visited[x][y] = true;
 		
 		while (!q.isEmpty()) {
 			Point p = q.poll();
 			for (int i = 0; i < 4; i++) {
 				int nx = p.x + dx[i];
 				int ny = p.y + dy[i];
-				if (outOfBounds(nx, ny) || visited[nx][ny]) continue;
-				if (map[nx][ny] == 1) continue;
+				if (nx == M) return true;
+				if (outOfBounds(nx, ny)) continue;
+				if (visited[nx][ny] || map[nx][ny] == 1) continue;
 				visited[nx][ny] = true;
 				q.offer(new Point(nx, ny));
 			}
 		}
+		return false;
 	}
 	
 	public static boolean outOfBounds(int x, int y) {
 		if (x < 0 || x >= M || y < 0 || y >= N) return true;
 		return false;
-	}
-	
-	public static String check() {
-		for (int i = 0; i < N; i++) {
-			if (visited[M - 1][i]) {
-				return "YES";
-			}
-		}
-		return "NO";
 	}
 	
 }
