062c8af13291830c67c540c9a466cff343bab6d9
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-02-09 14:57:34 +0900

    [Gold V] Title: 두 용액, Time: 412 ms, Memory: 30400 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static class Info {
+		long x; 
+		long y;
+		long v;
+		Info(long x, long y, long v) {
+			this.x = x;
+			this.y = y;
+			this.v = v;
+		}
+	}
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	static int N, result;
+	static int[] arr;
+	static Info info = new Info(0, 0, Integer.MAX_VALUE);
+	
+	public static void main(String[] args) throws IOException {
+		init();
+		solve();
+	}
+
+	public static void init() throws IOException {
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+	}
+	
+	public static void solve() {
+		Arrays.sort(arr);
+		
+		long sum = 0;
+		int start = 0, end = N - 1;
+		while (start < end) {
+			sum = arr[start] + arr[end];
+			if (Math.abs(sum) < info.v) {
+				info = new Info(start, end, Math.abs(sum));
+			}
+			if (sum < 0) {
+				start++;
+			} else {
+				end--;
+			}
+		}
+		
+		System.out.println(arr[(int) info.x] + " " + arr[(int) info.y]);
+	}
+	
+}
