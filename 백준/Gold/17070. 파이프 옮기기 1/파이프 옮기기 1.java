a75ed09c2de106ee0445b56377c8d1a6e4f55da2
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-02-20 00:13:37 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 316 ms, Memory: 15944 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -4,43 +4,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
 	static int N, result;
 	static int[][] map;
-	static int[] dx = {0, 1, 1}, dy = {1, 0, 1};
-
-	private static void dfs(int x, int y, int d) {
-		if (x == N - 1 && y == N - 1) {
-			result++;
-			return;
-		}
-		
-		for (int i = 0; i < 3; i++) {
-			if ((d == 0 && i == 1) || (d == 1 && i == 0)) continue;
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if (!isMove(nx, ny, i)) continue;
-			dfs(nx, ny, i);
-		}
-	}
-	
-	private static boolean isMove(int x, int y, int d) {
-		if (x < 0 || x >= N || y < 0 || y >= N) return false;
-		if ((d == 0 || d == 1) && map[x][y] != 0) return false;
-		if (d == 2) {
-			int nx = x - dx[d], ny = y - dy[d];
-			if (nx + 1 >= N || ny + 1 >= N) return false;
-			if (map[nx + 1][ny] != 0 || map[nx][ny + 1] != 0 || map[nx + 1][ny + 1] != 0) return false;		
-		}	
-		return true;
-	}
+	static int[][][] dir = { 
+				{ { 0, 1, 0 }, { 1, 1, 2 } }, 
+				{ { 1, 0, 1 }, { 1, 1, 2 } },
+				{ { 0, 1, 0 }, { 1, 0, 1 }, { 1, 1, 2 } } 
+			};
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		
+		init();
+		result = 0;
+		dfs(0, 1, 0);
+		System.out.println(result);
+	}
+
+	public static void init() throws IOException {
 		N = Integer.parseInt(br.readLine());
-		
 		map = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -48,10 +32,29 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+	}
 
-		result = 0;
-		dfs(0, 1, 0);
-		System.out.println(result);
+	public static void dfs(int x, int y, int d) {
+		if (x == N - 1 && y == N - 1) {
+			result++;
+			return;
+		}
+		
+		int nx, ny, nd;
+		for (int i = 0; i < dir[d].length; i++) {
+			nx = x + dir[d][i][0]; 
+			ny = y + dir[d][i][1]; 
+			nd = dir[d][i][2];
+			if (!isIn(nx, ny) || map[nx][ny] == 1) continue;
+			if (nd == 2 && (map[x + 1][y] == 1 || map[x][y + 1] == 1)) continue;
+			dfs(nx, ny, nd);					
+		}
+		
+	}
+	
+	public static boolean isIn(int x, int y) {
+		if (x < 0 || x >= N || y < 0 || y >= N) return false;
+		return true;
 	}
 
 }
