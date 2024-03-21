b77c170121b9518163b539a0858fea7ef936ff65
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-03-21 22:11:19 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,8 +1,4 @@
 def solution(data, ext, val_ext, sort_by):
-    info = {"code" : 0, "date" : 1, "maximum" : 2, "remain" : 3}
-    
-    result = []
-    for d in data:
-        if d[info[ext]] < val_ext:
-            result.append(d)
-    return sorted(result, key=lambda x : x[info[sort_by]])    
+    column = {"code" : 0, "date" : 1, "maximum" : 2, "remain" : 3}
+    result = [item for item in data if item[column[ext]] < val_ext]
+    return sorted(result, key=lambda x : x[column[sort_by]])    
