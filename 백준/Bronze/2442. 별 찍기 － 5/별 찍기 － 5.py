30ea50bd5a6e9c727718bd5f3b20a3419755cabe
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-04-22 22:35:40 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.py
@@ -0,0 +1,12 @@
+from sys import stdin
+input = stdin.readline
+
+
+def solve():
+    N = int(input())
+    for i in range(N):
+        print((' ' * (N - i - 1)) + ('*' * (2 * (i + 1) - 1)))
+
+
+if __name__ == "__main__":
+    solve()
