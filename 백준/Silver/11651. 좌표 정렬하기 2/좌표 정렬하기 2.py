07a1a8575a29ec457b835cc10ca9688ca1a8dcde
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-07-06 23:08:32 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 284 ms, Memory: 54732 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,19 @@
+from sys import stdin
+input = stdin.readline
+
+
+def solve():
+    N = int(input())
+
+    point = []
+    for _ in range(N):
+        x, y = map(int, input().split())
+        point.append([x, y])
+
+    result = sorted(point, key=lambda x: (x[1], x[0]))
+    for r in result:
+        print(*r)
+
+
+if __name__ == "__main__":
+    solve()
