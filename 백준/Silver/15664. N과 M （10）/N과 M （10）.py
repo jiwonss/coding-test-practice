fe6c98d971ff7183a5bd6dd452905dc1fa4eb17a
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-07-12 00:57:33 +0900

    [Silver II] Title: N과 M (10), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,31 @@
+from sys import stdin
+input = stdin.readline
+
+
+def backtracking(start, k):
+    if k == M:
+        print(*result)
+        return
+
+    t = 0
+    for i in range(start, N):
+        if visited[i] or t == arr[i]:
+            continue
+        t = arr[i]
+        visited[i] = 1
+        result[k] = arr[i]
+        backtracking(i + 1, k + 1)
+        visited[i] = 0
+
+
+def solve():
+    global N, M, arr, visited, result
+    N, M = map(int, input().split())
+    arr = sorted(list(map(int, input().split())))
+
+    visited, result = [0] * N, [0] * M
+    backtracking(0, 0)
+
+
+if __name__ == "__main__":
+    solve()
