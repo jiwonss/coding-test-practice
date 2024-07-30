c0b7c0f8699fcc47f003089a5863ce7a04298c57
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-07-31 00:03:49 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 772 ms, Memory: 133168 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -2,10 +2,16 @@ from sys import stdin
 from collections import Counter
 input = stdin.readline
 
-n = int(input())
-card = Counter(list(map(int, input().split())))
-m = int(input())
-arr = list(map(int, input().split()))
 
-for a in arr:
-    print(card[a], end=' ')
+def solve():
+    N = int(input())
+    card = Counter(list(map(int, input().split())))
+    M = int(input())
+    num = list(map(int, input().split()))
+
+    for n in num:
+        print(card[n], end=' ')
+
+
+if __name__ == "__main__":
+    solve()
