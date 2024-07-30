2699ed28f51e414bbbca8444ac9e0e77785ad997
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-07-30 22:25:58 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,10 +1,18 @@
 from sys import stdin
 input = stdin.readline
 
-n = int(input())
-cost = [list(map(int, input().split())) for _ in range(n)]
-for i in range(1, n):
-    cost[i][0] += min(cost[i - 1][1], cost[i - 1][2])
-    cost[i][1] += min(cost[i - 1][0], cost[i - 1][2])
-    cost[i][2] += min(cost[i - 1][0], cost[i - 1][1])
-print(min(cost[n - 1]))
+
+def solve():
+    N = int(input())
+    cost = [list(map(int, input().split())) for _ in range(N)]
+
+    for i in range(1, N):
+        cost[i][0] += min(cost[i - 1][1], cost[i - 1][2])
+        cost[i][1] += min(cost[i - 1][0], cost[i - 1][2])
+        cost[i][2] += min(cost[i - 1][0], cost[i - 1][1])
+
+    print(min(cost[N - 1]))
+
+
+if __name__ == "__main__":
+    solve()
