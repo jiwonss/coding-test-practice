a71066308e92c370dd613a06720cd65be6850207
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-08-04 03:32:10 +0900

    [Gold V] Title: 상범 빌딩, Time: 124 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
@@ -2,50 +2,57 @@ from sys import stdin
 from collections import deque
 input = stdin.readline
 
-dx = [1, 0, -1, 0, 0, 0]
-dy = [0, 1, 0, -1, 0, 0]
-dz = [0, 0, 0, 0, 1, -1]
+dx, dy, dz = [1, 0, -1, 0, 0, 0], [0, 1, 0, -1, 0, 0], [0, 0, 0, 0, 1, -1]
 
-while True:
-    L, R, C = map(int, input().split())
 
-    if L == 0 and R == 0 and C == 0:
-        break
+def out_of_bounds(x, y, z):
+    return not (0 <= x < R and 0 <= y < C and 0 <= z < L)
 
-    q = deque()
-    building = [[] for _ in range(L)]
-    end = []
-    for k in range(L):
-        for i in range(R):
-            building[k].append(list(input().strip()))
-            if 'S' in building[k][i]:
-                for j in range(C):
-                    if building[k][i][j] == 'S':
-                        q.append([k, i, j, 0])
-                        break
-            elif 'E' in building[k][i]:
-                for j in range(C):
-                    if building[k][i][j] == 'E':
-                        building[k][i][j] = '.'
-                        end.append([k, i, j])
-                        break
-        input()
 
-    check = 0
+def bfs():
     while q:
-        z, x, y, s = q.popleft()
-        if [z, x, y] == end[0]:
-            check = 1
-            print(f'Escaped in {s} minute(s).')
-            break
+        x, y, z, time = q.popleft()
         for d in range(6):
             nx = x + dx[d]
             ny = y + dy[d]
             nz = z + dz[d]
-            if 0 <= nx < R and 0 <= ny < C and 0 <= nz < L:
-                if building[nz][nx][ny] == '.':
-                    building[nz][nx][ny] = building[z][x][y]
-                    q.append([nz, nx, ny, s + 1])
-    
-    if not check:
-        print('Trapped!')
+            if out_of_bounds(nx, ny, nz):
+                continue
+            if visited[nz][nx][ny] or building[nz][nx][ny] == '#':
+                continue
+            if building[nz][nx][ny] == 'E':
+                return time + 1
+            visited[nz][nx][ny] = 1
+            q.append([nx, ny, nz, time + 1])
+    return 0
+
+
+def solve():
+    global L, R, C, building, q, visited
+
+    while True:
+        L, R, C = map(int, input().split())
+
+        if L == R == C == 0:
+            return
+
+        building = [[] for _ in range(L)]
+        q, visited = deque(), [[[0 for _ in range(C)] for _ in range(R)] for _ in range(L)]
+        for z in range(L):
+            for x in range(R):
+                building[z].append(list(input().strip()))
+                for y in range(C):
+                    if building[z][x][y] == 'S':
+                        visited[z][x][y] = 1
+                        q.append([x, y, z, 0])
+            input()
+
+        result = bfs()
+        if result:
+            print(f'Escaped in {result} minute(s).')
+        else:
+            print('Trapped!')
+
+
+if __name__ == "__main__":
+    solve()
