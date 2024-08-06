de2881450ae8280ed13d19b78acd906576892a1c
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-08-07 03:40:28 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 208 ms, Memory: 49648 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -0,0 +1,19 @@
+import math
+
+s = ["  *  ", " * * ", "*****"]
+
+
+def fractal(shift):
+    c = len(s)
+    for i in range(c):
+        s.append(s[i] + " " + s[i])
+        s[i] = ("   " * shift + s[i] + "   " * shift)
+
+
+n = int(input())
+k = int(math.log(int(n/3), 2))
+for i in range(k):
+    fractal(int(pow(2, i)))
+
+for i in range(n):
+    print(s[i])
