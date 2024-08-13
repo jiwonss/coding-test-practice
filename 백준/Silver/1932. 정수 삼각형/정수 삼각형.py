418167140f1d4b2db34942cff925b6017261a496
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-08-14 00:36:28 +0900

    [Silver II] Title: 정수 삼각형, Time: 100 ms, Memory: 36528 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,9 +1,18 @@
 from sys import stdin
 input = stdin.readline
 
-n = int(input())
-triangle = [list(map(int, input().split())) + [0] * (n - i - 1) for i in range(n)]
-for i in range(1, n):
-    for j in range(n):
-        triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
-print(max(triangle[n - 1]))
+
+def solve():
+    n = int(input())
+    triangle = [list(map(int, input().split())) + [0] * (n - i - 1) for i in range(n)]
+
+    for i in range(1, n):
+        for j in range(i + 1):
+            triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
+
+    result = max(triangle[n - 1])
+    print(result)
+
+
+if __name__ == "__main__":
+    solve()
