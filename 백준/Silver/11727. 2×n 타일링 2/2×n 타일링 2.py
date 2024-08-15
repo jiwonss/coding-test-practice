7a9f8c1618723a0c7644f8c4187b621adc861eb8
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-08-15 23:19:53 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,9 +1,15 @@
 from sys import stdin
 input = stdin.readline
 
-n = int(input())
-dp = [0] * 1001
-dp[1], dp[2] = 1, 3
-for i in range(3, n + 1):
-    dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007
-print(dp[n])
+
+def solve():
+    n = int(input())
+
+    dp = [0, 1, 3]
+    for i in range(3, n + 1):
+        dp.append((dp[i - 1] + (dp[i - 2] * 2)) % 10007)
+    print(dp[n])
+
+
+if __name__ == "__main__":
+    solve()
