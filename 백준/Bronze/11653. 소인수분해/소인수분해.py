bf83036e8113f7e5076f34997719f7091453cd3c
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-08-18 23:09:21 +0900

    [Bronze I] Title: 소인수분해, Time: 488 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -5,16 +5,12 @@ input = stdin.readline
 def solve():
     N = int(input())
 
-    if N == 1:
-        return
-
-    k, root = 2, N ** 0.5
-    while N != 1 or k < root:
-        while N % k == 0:
-            print(k)
-            N //= k
-        else:
-            k += 1
+    while N != 1:
+        for k in range(2, N + 1):
+            if N % k == 0:
+                print(k)
+                N //= k
+                break
 
 
 if __name__ == "__main__":
