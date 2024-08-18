398cef05ed269819b0db51b15e4c09b4b92113ef
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-08-19 01:18:14 +0900

    [Gold IV] Title: Puyo Puyo, Time: 52 ms, Memory: 34164 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -2,59 +2,86 @@ from sys import stdin
 from collections import deque
 input = stdin.readline
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-board = [list(input().strip()) for _ in range(12)]
+dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
 
-def bfs(a, b):
-    q = deque()
-    q.append((a, b))
-    temp.append((a, b))
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < 12 and 0 <= ny < 6:
-                if board[nx][ny] == board[a][b] and not visited[nx][ny]:
-                    q.append((nx, ny))
-                    temp.append((nx, ny))
-                    visited[nx][ny] = 1
 
-def down():
-    for i in range(6):
-        for j in range(10, -1, -1):
-            for k in range(11, j, -1):
-                if board[j][i] != '.' and board[k][i] == '.':
-                    board[k][i] = board[j][i]
-                    board[j][i] = '.'
+def drop():
+    for j in range(M):
+        temp = []
+        for i in range(N):
+            if field[i][j] != '.':
+                temp.append(field[i][j])
+                field[i][j] = '.'
+        if temp:
+            for i in range(N - 1, -1, -1):
+                if field[i][j] == '.':
+                    field[i][j] = temp.pop()
+                if not temp:
                     break
 
 
-def delete(temp):
-    for x, y in temp:
-        board[x][y] = '.'
+def delete():
+    for x, y in delete_list:
+        field[x][y] = '.'
 
-answer = 0
-while True:
-    flag = 0
-    visited = [[0] * 6 for _ in range(12)]
-    
-    for i in range(12):
-        for j in range(6):
-            if board[i][j] != '.' and not visited[i][j]:
-                visited[i][j] = 1
-                temp = []
-                bfs(i, j)
 
-                if len(temp) >= 4:
-                    flag = 1
-                    delete(temp)
+def out_of_bounds(x, y):
+    return not (0 <= x < N and 0 <= y < M)
 
-    if not flag:        
-        break
 
-    down()
-    answer += 1
-    
-print(answer)
+def bfs(i, j):
+    q.append([i, j])
+    visited[i][j] = 1
+
+    cnt, temp = 1, []
+    while q:
+        x, y = q.popleft()
+        temp.append([x, y])
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if out_of_bounds(nx, ny):
+                continue
+            if visited[nx][ny] or field[nx][ny] != field[i][j]:
+                continue
+            cnt += 1
+            visited[nx][ny] = 1
+            q.append([nx, ny])
+    return [cnt, temp]
+
+
+def explode():
+    global q, visited, delete_list
+
+    is_explode = 0
+    q, visited = deque(), [[0] * M for _ in range(N)]
+    delete_list = []
+    for i in range(N - 1, -1, -1):
+        for j in range(M):
+            if visited[i][j] or field[i][j] == '.':
+                continue
+            cnt, temp = bfs(i, j)
+            if cnt >= 4:
+                is_explode = 1
+                delete_list.extend(temp)
+    return is_explode
+
+
+def solve():
+    global N, M, field
+
+    N, M = 12, 6
+    field = [list(input().strip()) for _ in range(N)]
+
+    result = 0
+    while True:
+        if not explode():
+            break
+        result += 1
+        delete()
+        drop()
+    print(result)
+
+
+if __name__ == "__main__":
+    solve()
