afdcfb1123727de092971ff09c78a90b9458875a
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-08-24 21:27:54 +0900

    [Gold III] Title: 소수의 연속합, Time: 1140 ms, Memory: 75968 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -2,36 +2,37 @@ from sys import stdin
 from math import sqrt
 input = stdin.readline
 
-def prime_number(x):
-    for i in range(2, int(sqrt(x)) + 1):
+
+def get_prime_number_list(n):
+    arr = [1] * (n + 1)
+    arr[0] = arr[1] = 0
+    for i in range(2, int(sqrt(n)) + 1):
         if arr[i]:
             j = 2
             while i * j <= n:
                 arr[i * j] = 0
                 j += 1
-                
-    for i in range(2, x + 1):
-        if arr[i]:
-            num.append(i)
+    return [i for i in range(2, n + 1) if arr[i]]
 
-n = int(input())
-arr = [1] * (n + 1)
 
-num = []
-prime_number(n)
-num += [0]
-left, right = 0, 0
-t = num[0]
-answer = 0
-while  right < len(num) - 1:
-    if t < n:
-        right += 1
-        t += num[right]
-    else:
-        if t == n:
-            answer += 1
+def solve():
+    N = int(input())
+
+    num = [0] + get_prime_number_list(N)
+    for i in range(1, len(num)):
+        num[i] += num[i - 1]
+
+    left, right, result = 0, 0, 0
+    while right < len(num):
+        total = num[right] - num[left]
+        if total == N:
+            result += 1
+        if total < N:
             right += 1
-            t += num[right]
-        t -= num[left]
-        left += 1
-print(answer)
+        else:
+            left += 1
+    print(result)
+
+
+if __name__ == "__main__":
+    solve()
