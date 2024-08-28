d29b3288ba5e4a52a27f829eadfa0db8c3931d64
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-08-28 14:49:47 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,15 @@
+from sys import stdin
+input = stdin.readline
+
+
+def solve():
+    n = int(input())
+
+    dp = [0, 1]
+    for i in range(2, n + 1):
+        dp.append(dp[i - 1] + dp[i - 2])
+    print(dp[n])
+
+
+if __name__ == "__main__":
+    solve()
