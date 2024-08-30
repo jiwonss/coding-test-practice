213ea3d897f51036c4051ef4c85ea167790814c2
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-08-31 00:39:49 +0900

    [Gold II] Title: 보석 도둑, Time: 1532 ms, Memory: 108148 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,17 +1,23 @@
-import heapq
 from sys import stdin
+from heapq import heappush, heappop
 input = stdin.readline
 
-n, k = map(int, input().split())
-jewelry_list = sorted([list(map(int, input().split())) for _ in range(n)])
-bag_list = sorted([int(input()) for _ in range(k)])
 
-heap = []
-answer = 0
-for bag in bag_list:
-    while jewelry_list and bag >= jewelry_list[0][0]:
-        heapq.heappush(heap, -jewelry_list[0][1])
-        heapq.heappop(jewelry_list)
-    if heap:
-        answer += heapq.heappop(heap)
-print(-answer)
+def solve():
+    N, K = map(int, input().split())
+
+    jewel = sorted([list(map(int, input().split())) for _ in range(N)])
+    bag = sorted([int(input()) for _ in range(K)])
+
+    heap, result = [], 0
+    for i in range(K):
+        while jewel and jewel[0][0] <= bag[i]:
+            heappush(heap, -jewel[0][1])
+            heappop(jewel)
+        if heap:
+            result += -heappop(heap)
+    print(result)
+
+
+if __name__ == "__main__":
+    solve()
