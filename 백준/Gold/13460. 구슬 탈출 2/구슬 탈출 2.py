cee47b15077d147b5d5d3df6dd350adb5610c802
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-09-09 02:31:10 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 60 ms, Memory: 34164 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -2,54 +2,71 @@ from sys import stdin
 from collections import deque
 input = stdin.readline
 
-n, m = map(int, input().split())
-board = []
-for i in range(n):
-    board.append(list(input().strip()))
-    if 'R' or 'B' in board[i]:
-        for j in range(m):
-            if board[i][j] == 'R':
-                rx, ry = i, j
-            if board[i][j] == 'B':
-                bx, by = i, j
-q = deque()
-visited = [[[[0] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
+dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
 
-q.append([rx, ry, bx, by, 1])
-visited[rx][ry][bx][by] = 1
-
-def move(x, y, dx, dy):
+def roll(i, x, y):
     cnt = 0
-    while board[x + dx][y + dy] != '#' and board[x][y] != 'O':
-        x += dx
-        y += dy
+    while board[x + dx[i]][y + dy[i]] != '#' and board[x][y] != 'O':
+        x += dx[i]
+        y += dy[i]
         cnt += 1
-    return x, y, cnt
+    return [x, y, cnt]
 
-def bfs():
+
+def tilt(rx, ry, bx, by):
+    q = deque()
+    visited = [[[[0] * M for _ in range(N)] for _ in range(M)] for _ in range(N)]
+
+    q.append([rx, ry, bx, by, 1])
+    visited[rx][ry][bx][by] = 1
+
     while q:
         rx, ry, bx, by, cnt = q.popleft()
+
         if cnt > 10:
-            break
+            return -1
+
         for i in range(4):
-            nrx, nry, rcnt = move(rx, ry, dx[i], dy[i])
-            nbx, nby, bcnt = move(bx, by, dx[i], dy[i])
+            nrx, nry, rcnt = roll(i, rx, ry)
+            nbx, nby, bcnt = roll(i, bx, by)
+
             if board[nbx][nby] == 'O':
                 continue
             if board[nrx][nry] == 'O':
-                print(cnt)
-                return
-            if nrx == nbx and nry == nby:
+                return cnt
+
+            if [nrx, nry] == [nbx, nby]:
                 if rcnt > bcnt:
                     nrx, nry = nrx - dx[i], nry - dy[i]
                 else:
                     nbx, nby = nbx - dx[i], nby - dy[i]
-            if not visited[nrx][nry][nbx][nby]:
-                visited[nrx][nry][nbx][nby] = 1
-                q.append([nrx, nry, nbx, nby, cnt + 1])
-    print(-1)
 
-bfs()
+            if visited[nrx][nry][nbx][nby]:
+                continue
+
+            visited[nrx][nry][nbx][nby] = 1
+            q.append([nrx, nry, nbx, nby, cnt + 1])
+    return -1
+
+
+def solve():
+    global N, M, board
+
+    N, M = map(int, input().split())
+
+    board = []
+    rx, ry, bx, by = 0, 0, 0, 0
+    for i in range(N):
+        board.append(list(input().strip()))
+        for j in range(M):
+            if board[i][j] == 'R':
+                rx, ry = i, j
+            if board[i][j] == 'B':
+                bx, by = i, j
+
+    print(tilt(rx, ry, bx, by))
+
+
+if __name__ == "__main__":
+    solve()
