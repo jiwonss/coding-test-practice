fd8d9f8082760bfafdbd23c466b132f5d8f03502
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-09-12 21:54:29 +0900

    [Silver II] Title: 과자 나눠주기, Time: 1416 ms, Memory: 140192 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -1,19 +1,32 @@
 from sys import stdin
 input = stdin.readline
 
-m, n = map(int, input().split())
-arr = list(map(int, input().split()))
 
-answer = 0
-start, end = 1, max(arr)
-while start <= end:
-    mid = (start + end) // 2
+def count(n):
     cnt = 0
-    for i in range(n):
-        cnt += arr[i] // mid
-    if cnt >= m:
-        answer = mid
-        start = mid + 1
-    else:
-        end = mid - 1
-print(answer)
+    for l in L:
+        cnt += l // n
+    return cnt
+
+
+def solve():
+    global N, L
+
+    M, N = map(int, input().split())
+    L = list(map(int, input().split()))
+
+    start, end, result = 1, max(L), 0
+    while start <= end:
+        mid = (start + end) // 2
+
+        if count(mid) < M:
+            end = mid - 1
+        else:
+            result = mid
+            start = mid + 1
+
+    print(result)
+
+
+if __name__ == "__main__":
+    solve()
