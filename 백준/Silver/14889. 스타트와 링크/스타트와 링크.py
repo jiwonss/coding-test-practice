fcafdfa71907a94f626c9825c711e48553542415
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-09-17 00:51:01 +0900

    [Silver I] Title: 스타트와 링크, Time: 2428 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,34 +1,51 @@
-n = int(input())
-s = [list(map(int, input().split())) for _ in range(n)]
+from sys import stdin
 
-arr = [0] * n
-isused = [0] * n
-result = []
+input = stdin.readline
 
-def divide_team(cnt):
-    if cnt == (n // 2):
-        start = [arr[i] for i in range(n // 2)]
-        link = [i for i in range(n) if not isused[i]]
-        result.append(get_ability([start, link]))
-        return 
 
-    for i in range(n):
-        if not isused[i] and arr[cnt - 1] < i + 1:
-            arr[cnt] = i
-            isused[i] = 1
-            divide_team(cnt + 1)
-            isused[i] = 0
+def get_ability(team):
+    ability = 0
+    for i in range(N // 2 - 1):
+        for j in range(i + 1, N // 2):
+            ability += S[team[i]][team[j]] + S[team[j]][team[i]]
+    return ability
 
-def get_ability(t):
-    start, link = 0, 0
-    for i in range((n // 2) - 1):
-        for j in range(i + 1, n // 2):
-            start_i, start_j = t[0][i], t[0][j]
-            link_i, link_j = t[1][i], t[1][j]
-            start += s[start_i][start_j] + s[start_j][start_i]
-            link += s[link_i][link_j] + s[link_j][link_i]
-    return abs(start - link)
 
-divide_team(0)
-print(min(result))
+def diff(start, link):
+    return abs(get_ability(start) - get_ability(link))
 
+
+def assign(k, idx):
+    global result
+
+    if k == N // 2:
+        start, link = [], []
+        for i in range(N):
+            if visited[i]:
+                start.append(i)
+            else:
+                link.append(i)
+        result = min(result, diff(start, link))
+        return
+
+    for i in range(idx, N):
+        if visited[i]:
+            continue
+        visited[i] = 1
+        assign(k + 1, i + 1)
+        visited[i] = 0
+
+
+def solve():
+    global N, S, visited, result
+
+    N = int(input())
+    S = [list(map(int, input().split())) for _ in range(N)]
+
+    visited, result = [0] * N, 1001
+    assign(0, 0)
+    print(result)
+
+
+if __name__ == "__main__":
+    solve()
