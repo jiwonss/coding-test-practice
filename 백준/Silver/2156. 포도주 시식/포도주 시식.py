742b6ad9f9bf1464f6708c221326add5ef14ddbe
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-10-07 00:41:38 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,14 +1,25 @@
-n = int(input())
-w = [0] + [int(input()) for _ in range(n)]
+from sys import stdin
 
-if n == 1:
-    print(w[1])
-    exit()
+input = stdin.readline
 
-dp = [0] * (n + 1)
 
-dp[1], dp[2] = w[1], w[1] + w[2]
-for i in range(3, n + 1):
-    dp[i] = max(dp[i - 1], dp[i - 3] + w[i - 1] + w[i], dp[i - 2] + w[i])
+def solve():
+    n = int(input())
+    wine = [0] + [int(input()) for _ in range(n)]
 
-print(dp[n])
+    if n == 1:
+        print(wine[1])
+        return
+
+    dp = [0] * (n + 1)
+    dp[1], dp[2] = wine[1], wine[1] + wine[2]
+
+    for i in range(3, n + 1):
+        dp[i] = max(dp[i - 1], dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i])
+
+    result = dp[n]
+    print(result)
+
+
+if __name__ == "__main__":
+    solve()
