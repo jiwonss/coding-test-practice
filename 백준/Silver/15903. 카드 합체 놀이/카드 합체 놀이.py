ab73fbdf9b128f032163d448e95640e678b31c60
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-10-08 01:43:40 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 48 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -1,17 +1,19 @@
 from sys import stdin
+from heapq import heapify, heappush, heappop
 
 input = stdin.readline
 
 
 def solve():
     n, m = map(int, input().split())
-    status = sorted(list(map(int, input().split())))
+    status = list(map(int, input().split()))
+
+    heapify(status)
 
     for _ in range(m):
-        v = status[0] + status[1]
-        status[0] = v
-        status[1] = v
-        status.sort()
+        v = heappop(status) + heappop(status)
+        heappush(status, v)
+        heappush(status, v)
 
     result = sum(status)
     print(result)
