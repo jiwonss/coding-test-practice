f1f560ae0a52dd8c5ff8ae4b590817c2170d8359
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-10-20 20:26:35 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 572 ms, Memory: 50120 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.py
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.py
@@ -1,16 +1,25 @@
 from sys import stdin
 from collections import defaultdict
+
 input = stdin.readline
 
-m, n = map(int, input().split())
-universe = defaultdict(int)
-for _ in range(m):
-    planet = list(map(int, input().split()))
-    key = sorted(list(set(planet)))
-    rank = {key[i]: i for i in range(len(key))}
-    universe[tuple([rank[p] for p in planet])] += 1
 
-answer = 0
-for v in universe.values():
-    answer += v * (v - 1) // 2
-print(answer)
+def solve():
+    M, N = map(int, input().split())
+
+    universe = defaultdict(int)
+    for _ in range(M):
+        planet = list(map(int, input().split()))
+        value = sorted(set(planet))
+        compress_map = {v: i for i, v in enumerate(value)}
+        compress_arr = [compress_map[p] for p in planet]
+        universe[tuple(compress_arr)] += 1
+
+    result = 0
+    for v in universe.values():
+        result += v * (v - 1) // 2
+    print(result)
+
+
+if __name__ == "__main__":
+    solve()
