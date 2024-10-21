42f125270e813df0ffd675018f0ad1d4263fa74e
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2024-10-21 21:45:17 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 348 ms, Memory: 70708 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -1,36 +1,46 @@
-import heapq
 from sys import stdin
-input = stdin.readline
-INF = 1e9
+from heapq import heappush, heappop
 
-n = int(input())
-m = int(input())
-graph = [[] for _ in range(n + 1)]
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-start, end = map(int, input().split())
-distance = [INF] * (n + 1)
-path = [[] for _ in range(n + 1)]
+input = stdin.readline
+INF = float("inf")
+
 
 def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start, [start]))
+    heap = []
+    heappush(heap, [0, start, [start]])
     distance[start] = 0
-    while q:
-        d, now, p = heapq.heappop(q)
-        if distance[now] < d:
+
+    while heap:
+        d, node, p = heappop(heap)
+        if d > distance[node]:
             continue
-        for i in graph[now]:
-            cost = d + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                path[i[0]] = p + [i[0]]
-                heapq.heappush(q, (cost, i[0], p + [i[0]]))
+        for v in graph[node]:
+            cost = d + v[1]
+            if cost < distance[v[0]]:
+                distance[v[0]] = cost
+                path[v[0]] = p + [v[0]]
+                heappush(heap, [cost, v[0], p + [v[0]]])
 
-dijkstra(start)
 
-print(distance[end])
-print(len(path[end]))
-for p in path[end]:
-    print(p, end=' ')
+def solve():
+    global graph, distance, path
+
+    n = int(input())
+    m = int(input())
+
+    graph = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        graph[a].append([b, c])
+
+    start, end = map(int, input().split())
+    distance, path = [INF] * (n + 1), [[] for _ in range(n + 1)]
+    dijkstra(start)
+
+    print(distance[end])
+    print(len(path[end]))
+    print(*path[end])
+
+
+if __name__ == "__main__":
+    solve()
