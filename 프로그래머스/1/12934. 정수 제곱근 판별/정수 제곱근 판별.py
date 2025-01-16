a0ecd7a6f12396a72f249a50bb5dd99bd9d77f27
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-01-17 00:24:32 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,4 +1,5 @@
 def solution(n):
-    if (int(n ** 0.5) == n ** 0.5):
-        return (int(n ** 0.5) + 1) ** 2
+    sqrt = n ** 0.5
+    if (int(sqrt) == sqrt):
+        return (int(sqrt) + 1) ** 2
     return -1
