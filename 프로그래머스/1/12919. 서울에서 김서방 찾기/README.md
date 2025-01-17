d3d1ee93499405663844bc95ea2588ff4131fabf
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-01-17 18:27:01 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.15 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 서울에서 김서방 찾기 - 12919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 75.8 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 17일 18:19:47
+2025년 01월 17일 18:27:00
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int x = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                x = i; 
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("김서방은 ").append(String.valueOf(x)).append("에 있다");
+        return sb.toString();
+    }
+}
