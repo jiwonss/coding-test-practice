3ffb9e850e925db98777070cfd62a8cc6e402867
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-01-18 20:18:04 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        for (int n : numbers) {
+            answer -= n;
+        }
+        return answer;
+    }
+}
