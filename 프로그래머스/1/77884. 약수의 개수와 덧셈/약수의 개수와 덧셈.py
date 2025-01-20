78fbb4c82a67fd8c3a81dab2c1be8d8e3abd620d
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-01-20 17:18:28 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 3.92 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,8 +1,10 @@
-def get_divisor_count(k):
+def get_divisor_count(n):
     cnt = 0
-    for n in range(1, k + 1):
-        if k % n == 0:
+    for i in range(1, int(n ** 0.5) + 1):
+        if n % i == 0:
             cnt += 1
+            if i ** 2 != n:
+                cnt += 1
     return cnt
 
 def solution(left, right):
