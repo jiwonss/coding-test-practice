27ff0042f87dee1cc61a28d16df754c40b9a15e2
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-01-21 18:06:35 +0900

    [level 1] Title: 직사각형 별찍기, Time: 205.24 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/README.md
+++ 프로그래머스/1/12969. 직사각형 별찍기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 직사각형 별찍기 - 12969 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969?language=java) 
 
 ### 성능 요약
 
-메모리: 7.48 MB, 시간: 24.04 ms
+메모리: 68.3 MB, 시간: 205.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 21일 16:26:28
+2025년 01월 21일 18:06:22
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append("*");
+        }
+        
+        for (int i = 0; i < m; i++) {
+            System.out.println(sb.toString());
+        } 
+    }
+}
