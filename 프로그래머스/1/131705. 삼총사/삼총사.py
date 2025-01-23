190bf2c6cc37d9435dbb6d9894004b8c481519af
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-01-24 00:03:06 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+
+def solution(number):
+    answer = 0
+    for c in combinations(number, 3):
+        if sum(c) == 0:
+            answer += 1
+    return answer
