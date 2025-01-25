ad8875fa393922aa99fd0a2a37cf460ac2f75463
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-01-25 15:46:17 +0900

    [level 1] Title: 시저 암호, Time: 1.95 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,12 +1,15 @@
 class Solution {
     public String solution(String s, int n) {
         char[] c = s.toCharArray();
+        int lower = (int) 'a';
+        int upper = (int) 'A';
         for (int i = 0; i < s.length(); i++) {
+            int k = (int) c[i];
             if (Character.isLowerCase(c[i])) {
-                c[i] = (char) ((((int) c[i] - (int) 'a') + n) % 26 + (int) 'a');
+                c[i] = (char) ((k - lower + n) % 26 + lower);
             }
             if (Character.isUpperCase(c[i])) {
-                c[i] = (char) ((((int) c[i] - (int) 'A') + n) % 26 + (int) 'A');
+                c[i] = (char) ((k - upper + n) % 26 + upper);
             }
         }
         return String.valueOf(c);
