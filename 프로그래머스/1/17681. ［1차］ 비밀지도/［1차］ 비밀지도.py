02b026e0a7284ac179bc5127e2f64089fb3bbf9e
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-01-27 23:05:05 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,8 @@
+def solution(n, arr1, arr2):
+    answer = []
+    for a1, a2 in zip(arr1, arr2):
+        answer.append(
+            bin(a1 | a2)[2:]
+            .zfill(n).replace('0',' ').replace('1', '#')
+        )
+    return answer
