009e01b8b1955f9a4f6d9858252524927165ac26
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-01-27 20:57:07 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,15 +4,15 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         int i, j, k;
-        for (int a = 0; a < commands.length; a++) {
-            int[] command = commands[a];
-            i = command[0] - 1; j = command[1] - 1; k = command[2] - 1;
-            int[] arr = new int[j - i + 1];
-            for (int idx = i; idx < j + 1; idx++) {
-                arr[idx - i] = array[idx];
+        int idx = 0;
+        for (int[] command : commands) {
+            i = command[0]; j = command[1]; k = command[2];
+            List<Integer> list = new ArrayList<>();
+            for (int a = i - 1; a < j; a++) {
+                list.add(array[a]);
             }
-            Arrays.sort(arr);
-            answer[a] = arr[k];
+            Collections.sort(list);
+            answer[idx++] = list.get(k - 1);
         }
         return answer;
     }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 104 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 27일 20:49:08
+2025년 01월 27일 20:57:03
 
 ### 문제 설명
 
