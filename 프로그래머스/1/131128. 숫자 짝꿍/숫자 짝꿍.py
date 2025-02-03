47be48b8e51f3e97d002b4f80313ae44b926bb41
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-02-03 19:44:53 +0900

    [level 1] Title: 숫자 짝꿍, Time: 90.11 ms, Memory: 27.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,10 @@
+def solution(X, Y):
+    answer = ''
+    for i in range(9, -1, -1):
+        answer += str(i) * min(X.count(str(i)), Y.count(str(i)))
+    
+    if answer == '':
+        return '-1'
+    if len(answer) == answer.count('0'):
+        return '0'
+    return answer
