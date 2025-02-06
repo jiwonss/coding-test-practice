00252fd448b146665ccd869edb69c7347251a689
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-02-07 01:56:16 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.57 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,30 +1,3 @@
 def solution(numbers, hand):
-    keypad = [[1, 4, 7, -1], [2, 5, 8, 0], [3, 6, 9, -1]]
-    
     answer = ''
-    left, right = [0, 3], [2, 3] # [0] : 0, 1, 2 | [1] : 0, 1, 2, 3
-    for n in numbers:
-        if n in keypad[0]:
-            answer += 'L'
-            left = [0, keypad[0].index(n)]
-        if n in keypad[2]:
-            answer += 'R'
-            right = [2, keypad[2].index(n)]
-        if n in keypad[1]:
-            idx = keypad[1].index(n)
-            left_dist = abs(1 - left[0]) + abs(left[1] - idx)
-            right_dist = abs(1 - right[0]) + abs(right[1] - idx)
-            if left_dist == right_dist:
-                if hand == 'left':
-                    answer += 'L'
-                    left = [1, idx]
-                else:
-                    answer += 'R'
-                    right = [1, idx]
-            elif left_dist < right_dist:
-                answer += 'L'
-                left = [1, idx]
-            else:
-                answer += 'R'
-                right = [1, idx]
     return answer
