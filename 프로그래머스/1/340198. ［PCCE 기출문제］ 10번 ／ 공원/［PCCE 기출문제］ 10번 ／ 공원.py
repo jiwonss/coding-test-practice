cc24b30577b415a3d932e322b132effd11965fef
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-02-14 22:26:48 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 18.91 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,23 +1,25 @@
-def check(i, j, mat, park):
+def check(park, mat, i, j):
     for x in range(i, i + mat):
         for y in range(j, j + mat):
             if x >= n or y >= m or park[x][y] != '-1':
                 return 0
     return 1
 
+def spread(park, mat):
+    for i in range(n):
+        for j in range(m):
+            if park[i][j] != '-1':
+                continue
+            if check(park, mat, i, j):
+                answer.append(mat)
+                return
 
 def solution(mats, park):
-    global n, m
+    global n, m, answer
     
     n, m = len(park), len(park[0])
     
     answer = []
     for mat in mats:
-        for i in range(n):
-            for j in range(m):
-                if park[i][j] != '-1':
-                    continue
-                if check(i, j, mat, park):
-                    answer.append(mat)
-                    break
+        spread(park, mat)
     return max(answer) if answer else -1
