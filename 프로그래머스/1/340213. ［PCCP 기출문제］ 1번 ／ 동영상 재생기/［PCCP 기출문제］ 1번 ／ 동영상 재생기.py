7cab564833a216d948fdd81eec40c941aad58a53
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-02-16 04:17:35 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,31 +1,28 @@
 def solution(video_len, pos, op_start, op_end, commands):
-    video = list(map(int, video_len.split(":")))
-    video_time = video[0] * 60 + video[1]
+    video_split = list(map(int, video_len.split(":")))
+    video_time = video_split[0] * 60 + video_split[1];
     
-    op = list(map(int, op_start.split(":"))) + list(map(int, op_end.split(":")))
-    op_start_time, op_end_time = op[0] * 60 + op[1], op[2] * 60 + op[3]
-
-    p = list(map(int, pos.split(":")))
-    pos_time = p[0] * 60 + p[1]
+    op_split = [list(map(int, op_start.split(":"))), list(map(int, op_end.split(":")))]
+    op_start_time = op_split[0][0] * 60 + op_split[0][1]
+    op_end_time = op_split[1][0] * 60 + op_split[1][1]
     
-    idx = 0
-    while idx < len(commands):
-        if op_start_time <= pos_time <= op_end_time:
-            pos_time = op_end_time
-        
-        command = commands[idx]
+    pos_split = list(map(int, pos.split(":")))
+    pos_time = pos_split[0] * 60 + pos_split[1];
+    
+    if op_start_time <= pos_time <= op_end_time:
+        pos_time = op_end_time
+    
+    for command in commands:
         if command == 'prev':
             pos_time -= 10
-            if pos_time < 10:
+            if pos_time < 0:
                 pos_time = 0
         if command == 'next':
             pos_time += 10
-            if pos_time > video_time - 10:
+            if pos_time > video_time:
                 pos_time = video_time
-        idx += 1
-        
-    if op_start_time <= pos_time <= op_end_time:
+        if op_start_time <= pos_time <= op_end_time:
             pos_time = op_end_time
-            
+
     div, mod = map(str, divmod(pos_time, 60))
     return div.zfill(2) + ":" + mod.zfill(2)
