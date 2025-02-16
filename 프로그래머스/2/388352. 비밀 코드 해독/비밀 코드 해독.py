cbc53b7785a77d7d6fad624a126355aca918dde8
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-02-16 23:02:48 +0900

    [level 2] Title: 비밀 코드 해독, Time: 336.70 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+def solution(n, q, ans):
+    m = len(q)
+    
+    answer = 0
+    for c in combinations([i for i in range(1, n + 1)], 5):
+        for i in range(m):
+            if len(set(q[i]) & set(c)) != ans[i]:
+                break
+        else:
+            answer += 1       
+    return answer
