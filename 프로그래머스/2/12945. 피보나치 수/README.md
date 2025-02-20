76958383dba6c26cacc511c953e4dba76f3fad23
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-02-21 03:12:35 +0900

    [level 2] Title: 피보나치 수, Time: 2.41 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=java) 
 
 ### 성능 요약
 
-메모리: 13.8 MB, 시간: 24.36 ms
+메모리: 81 MB, 시간: 2.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 21일 03:11:06
+2025년 02월 21일 03:12:33
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int[] dp = new int[100001];
-        dp[1] = 1; dp[2] = 1;
+        int[] fibo = new int[n + 1];
+        fibo[1] = 1; fibo[2] = 1;
         
         for (int i = 3; i < n + 1; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
         }
-        
-        int answer = dp[n];
-        return answer;
+        return fibo[n];
     }
 }
