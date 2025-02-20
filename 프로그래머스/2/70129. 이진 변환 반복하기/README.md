cebdf344bae6f2ada3085c27d91175b1b7624929
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-02-20 23:16:59 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 8.94 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=java) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 2.76 ms
+메모리: 78.9 MB, 시간: 8.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 20일 19:26:13
+2025년 02월 20일 23:16:53
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = {0, 0};
-        while (true) {
-            if (s.length() == 1) {
-                break;
+        while (!s.equals("1")) {
+            int[] cnt = new int[2];
+            for (char c : s.toCharArray()) {
+                if (c == '0') cnt[0]++;
+                else cnt[1]++;
             }
             
-            int cnt0 = 0, cnt1 = 0;
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '0') {
-                    cnt0 += 1;
-                } else {
-                    cnt1 += 1;
-                }
-            }
-            answer[1] += cnt0;
+            s = Integer.toBinaryString(cnt[1]);
             
-            s = Integer.toBinaryString(cnt1);
-            answer[0] += 1;
+            answer[0]++;
+            answer[1] += cnt[0];
         }
         return answer;
     }
