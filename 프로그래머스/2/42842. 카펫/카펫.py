c8de06983a316da527a6661920b299d356be3185
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-02-25 23:21:28 +0900

    [level 2] Title: 카펫, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,4 +1,5 @@
 def solution(brown, yellow):
     for i in range(1, int(yellow ** 0.5) + 1):
-        if yellow % i == 0 and 2 * (i + yellow // i) == brown - 4:
-            return [yellow // i + 2, i + 2]
+        w, h = yellow // i, i
+        if yellow % h == 0 and 2 * (w + h) == brown - 4:
+            return [w + 2, h + 2]
