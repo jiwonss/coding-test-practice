208092fdc285ce5de3449a5434bdd9820e0757f0
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-03-08 21:39:41 +0900

    [level 2] Title: 행렬의 곱셈, Time: 46.66 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -0,0 +1,6 @@
+def solution(arr1, arr2):
+    answer = [[] for _ in range(len(arr1))]
+    for i, a_col in enumerate(arr1):
+        for b_col in zip(*arr2):
+            answer[i].append(sum([a * b for a, b in zip(a_col, b_col)]))
+    return answer
