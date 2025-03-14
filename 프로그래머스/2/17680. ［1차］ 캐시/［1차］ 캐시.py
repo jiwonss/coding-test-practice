28e6e71ccd37cc9251549e5a8583cc8a0b4eb0ec
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-03-14 22:59:45 +0900

    [level 2] Title: [1차] 캐시, Time: 64.12 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    cache, answer = deque(maxlen=cacheSize), 0
+    for city in cities:
+        city = city.lower()
+        if city in cache:
+            answer += 1
+            cache.remove(city)
+            cache.append(city)
+        else:
+            answer += 5
+            cache.append(city)
+    return answer
