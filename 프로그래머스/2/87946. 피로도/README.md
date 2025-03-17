cc043f761619f961387af1dee917b2791f10b578
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-03-17 19:42:10 +0900

    [level 2] Title: 피로도, Time: 62.71 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.06 MB, 시간: 45.46 ms
+메모리: 9.19 MB, 시간: 62.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 17일 19:31:18
+2025년 03월 17일 19:42:03
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,12 +1,12 @@
-def adventure(cnt, k, dungeons):
+def adventure(k, dungeons):
     global answer
-    answer = max(answer, cnt)
-    
+    answer = max(answer, sum(visited))
+
     for i in range(n):
         if visited[i] or k < dungeons[i][0]:
             continue
         visited[i] = 1
-        adventure(cnt + 1, k - dungeons[i][1], dungeons)
+        adventure(k - dungeons[i][1], dungeons)
         visited[i] = 0
         
 def solution(k, dungeons):
@@ -15,5 +15,5 @@ def solution(k, dungeons):
     
     visited = [0] * n
     answer = 0
-    adventure(0, k, dungeons)
+    adventure(k, dungeons)
     return answer
