43274d51a16576bf77599abfa6f5cdec6a80c867
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-03-21 01:36:29 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,25 +1,25 @@
 from sys import stdin
+
 input = stdin.readline
 
 
-def backtracking(start, k):
+def dfs(k, start):
     if k == M:
         print(*result)
         return
 
-    for i in range(start, N + 1):
-        result[k] = i
-        backtracking(i, k + 1)
+    for i in range(start, N):
+        result[k] = i + 1
+        dfs(k + 1, i)
 
 
 def solve():
-    global N, M, arr, result
+    global N, M, result
 
     N, M = map(int, input().split())
-    arr = [i for i in range(1, N + 1)]
 
     result = [0] * M
-    backtracking(1, 0)
+    dfs(0, 0)
 
 
 if __name__ == "__main__":
