f03dfaf33f661ba8559453e6f621a8c51633e575
Seo Jiwon <72875528+jiwonss@users.noreply.github.com>
2025-06-08 22:38:03 +0900

    [level 2] Title: 리코쳇 로봇, Time: 8.37 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -5,28 +5,27 @@ dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
 def out_of_bounds(x, y):
     return not (0 <= x < n and 0 <= y < m)
 
-def move(board, d, x, y):
+def move(board, x, y, d):
     while True:
-        x += dx[d]
-        y += dy[d]
-        if out_of_bounds(x, y) or board[x][y] == 'D':
-            break
-    x -= dx[d]
-    y -= dy[d]
-    return [x, y]
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if out_of_bounds(nx, ny) or board[nx][ny] == 'D':
+            return [x, y]
+        x, y = nx, ny
     
 def bfs(board):
     q = deque()
     visited = [[0] * m for _ in range(n)]
-
+    
     sx, sy = start
+    
     q.append([sx, sy, 0])
     visited[sx][sy] = 1
     
     while q:
         x, y, cnt = q.popleft()
         for d in range(4):
-            nx, ny = move(board, d, x, y)
+            nx, ny = move(board, x, y, d)
             if visited[nx][ny]:
                 continue
             if board[nx][ny] == 'G':
@@ -34,22 +33,16 @@ def bfs(board):
             visited[nx][ny] = 1
             q.append([nx, ny, cnt + 1])
     return -1
-    
+
 def solution(board):
     global n, m, start
-    
-    n, m = len(board), len(board[0])
-    
-    start = [-1, -1]
+
+    n, m, start = len(board), len(board[0]), [-1, -1]
     for i in range(n):
         for j in range(m):
             if board[i][j] == 'R':
                 start = [i, j]
                 break
-        if start !=  [-1, -1]:
+        if start != [-1, -1]:
             break
-
     return bfs(board)
-    
-                
-            
