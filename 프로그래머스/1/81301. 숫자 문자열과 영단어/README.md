e971725d2455cee364367f73c398fdb151bb775d
Jiwon Seo <72875528+jiwonss@users.noreply.github.com>
2025-10-08 00:48:11 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 88.6 MB, 시간: 0.14 ms
+메모리: 9.22 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 26일 22:38:26
+2025년 10월 08일 00:48:08
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,10 +1,10 @@
-def solution(s):
-    number = {
-        "one" : 1, "two" : 2, "three" : 3, "four" : 4, "five" : 5,
-        "six" : 6, "seven" : 7, "eight" : 8, "nine" : 9, "zero" : 0
+number = {
+    "zero": "0", "one": "1", "two": "2", "three": "3", "four": "4",
+    "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"
     }
-    
+
+def solution(s):
     answer = s
     for k, v in number.items():
-        answer = answer.replace(k, str(v))
+        answer = answer.replace(k, v)
     return int(answer)
