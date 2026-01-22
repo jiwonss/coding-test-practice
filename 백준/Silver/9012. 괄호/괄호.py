84c89261accfaafd3fd1a99617da770aa64e98b6
Jiwon Seo <72875528+jiwonss@users.noreply.github.com>
2026-01-22 22:06:00 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,26 +1,25 @@
 from sys import stdin
+
 input = stdin.readline
+
+
+def check(s):
+    stack = []
+    for i in range(len(s)):
+        if s[i] == "(":
+            stack.append(s[i])
+        else:
+            if not stack:
+                return "NO"
+            stack.pop()
+    return "NO" if stack else "YES"
 
 
 def solve():
     T = int(input())
     for _ in range(T):
-        ps = input().strip()
-
-        check, stack = 0, []
-        for s in ps:
-            if s == '(':
-                stack.append(s)
-            else:
-                if not stack:
-                    check = 1
-                    break
-                stack.pop()
-
-        if check or stack:
-            print('NO')
-        else:
-            print('YES')
+        s = input().strip()
+        print(check(s))
 
 
 if __name__ == "__main__":
